1e0119c940679d5ae14c85070213cf3b15226971
Yang Yang <yangyang.tue@gmail.com>
2020-12-04 15:14:22 +0100

    Initialization fixed

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -198,10 +198,7 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         int ux = (int) Math.ceil(dx);
         int uy = (int) Math.ceil(dy);
         int uz = (int) Math.ceil(dz);
-         
-//        double alpha = (dx - lx) / (ux - lx);
-//        double beta = (dy - ly) / (uy - ly);
-//        double gamma = (dz - lz) / (uz - lz);
+        
         double alpha = dx - lx;
         double beta = dy - ly;
         double gamma = dz - lz;
@@ -277,13 +274,9 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         int uy = (int) Math.ceil(dy);
         int uz = (int) Math.ceil(dz);
         
-//        double alpha = (dx - lx) / (ux - lx);
-//        double beta = (dy - ly) / (uy - ly);
-//        double gamma = (dz - lz) / (uz - lz);
         double alpha = dx - lx;
         double beta = dy - ly;
         double gamma = dz - lz;
-
         
         VoxelGradient g0 = gradients.getGradient(lx, ly, lz);
         VoxelGradient g1 = gradients.getGradient(ux, ly, lz);
